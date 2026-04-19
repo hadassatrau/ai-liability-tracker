cd4d058b264e5e2ccf3423aade2dd4267ad5b74f
hadassatrau <64993879+hadassatrau@users.noreply.github.com>
2026-04-19 14:31:37 -0400

    Update fpgi_monitor.py

--- fpgi_monitor.py
+++ fpgi_monitor.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain.output_parsers import ResponseSchema, StructuredOutputParser
 import datetime
 
